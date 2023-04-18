9815f66af3f52999207778c939c2318e01429a39
Bhimgouda <satvikp508@gmail.com>
2023-04-18 13:00:45 +0530

    added verify script

--- deploy/01-deploy-Token.js
+++ deploy/01-deploy-Token.js
@@ -1,2 +1,9 @@
 const {network, ethers} = require("hardhat");
+const {developmentChains, networkConfig} = require("../helper-hardhat.config")
+const {verify} = require("../utils/verify")
 
+// hre = hardhat runtime environment gives all this arguments to deploy scripts
+
+module.exports = ({getNamedAccounts, deploymnents}) => {
+    
+}
